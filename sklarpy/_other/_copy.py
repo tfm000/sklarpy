13e55515446d1e83f54a7c3d6ceed63b9c38721e
Tyler <tylerfmitchell@gmail.com>
2023-02-28 10:44:46 +0000

    creating base class allowing copying

---
+++ sklarpy/_other/_copy.py
@@ -0,0 +1,31 @@
+import copy
+
+
+__all__ = ['Copyable']
+
+
+class Copyable:
+    """Class used for copying"""
+    def copy(self, name: str = None):
+        """Returns a copy of your SklarPy object.
+
+        Parameters
+        ===========
+        name : str
+            The name of your copied object. If None, the name of the original is used.
+
+        Returns
+        ========
+        self:
+            self
+        """
+        if name is None:
+            new_name: str = self.name
+        elif isinstance(name, str):
+            new_name: str = name
+        else:
+            raise TypeError("name must be a string.")
+
+        copy_object = copy.deepcopy(self)
+        copy_object._name = new_name
+        return copy_object
