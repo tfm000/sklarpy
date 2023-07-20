4ecdbc84adb1137648d4bcb2ca858a6b0a7ecb61
tfm000 <tylerfmitchell1999@gmail.com>
2023-07-20 22:05:05 +0100

    remove modified bessel

--- sklarpy/misc/__init__.py
+++ sklarpy/misc/__init__.py
@@ -3,4 +3,3 @@ from sklarpy.misc.inverse_transform import inverse_transform
 from sklarpy.misc.goodness_of_fit import continuous_goodness_of_fit, discrete_goodness_of_fit
 from sklarpy.misc.correlation import CorrelationMatrix
 from sklarpy.misc.gradient import gradient_1d
-from sklarpy.misc.modified_bessel import kv_3
