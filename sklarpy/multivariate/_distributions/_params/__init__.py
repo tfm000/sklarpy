ff0b29ec18bef45974900f7118033ba13935774b
tfm000 <tylerfmitchell1999@gmail.com>
2023-07-20 22:16:21 +0100

    adding multivar sym gen hyperbolic

--- sklarpy/multivariate/_distributions/_params/__init__.py
+++ sklarpy/multivariate/_distributions/_params/__init__.py
@@ -1,3 +1,4 @@
 from sklarpy.multivariate._distributions._params._normal import MultivariateNormalParams
 from sklarpy.multivariate._distributions._params._student_t import MultivariateStudentTParams
 from sklarpy.multivariate._distributions._params._generalized_hyperbolic import MultivariateGenHyperbolicParams
+from sklarpy.multivariate._distributions._params._symmetric_generalized_hyperbolic import MultivariateSymGenHyperbolicParams
