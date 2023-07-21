dc7e2982869bc3f89939a89079837b9ed4a73d39
tfm000 <tylerfmitchell1999@gmail.com>
2023-07-21 02:38:41 +0100

    adding hyperbolic and nig dists

--- sklarpy/multivariate/_distributions/_hyperbolics.py
+++ sklarpy/multivariate/_distributions/_hyperbolics.py
@@ -1,25 +1,30 @@
 import numpy as np
 from typing import Tuple, Union
 from scipy.optimize import differential_evolution
+from abc import abstractmethod
 
 from sklarpy.multivariate._distributions._generalized_hyperbolic import multivariate_gen_hyperbolic_gen
 from sklarpy.multivariate._prefit_dists import PreFitContinuousMultivariate
 from sklarpy._other import Params
-from sklarpy.multivariate._distributions._params import MultivariateMarginalHyperbolicParams
+from sklarpy.multivariate._distributions._params import MultivariateMarginalHyperbolicParams, MultivariateHyperbolicParams, MultivariateNIGParams
 
 
-__all__ = ['multivariate_marginal_hyperbolic']
+__all__ = ['multivariate_marginal_hyperbolic', 'multivariate_hyperbolic', 'multivariate_nig']
 
 
-class multivariate_marginal_hyperbolic_gen(multivariate_gen_hyperbolic_gen):
+class multivariate_hyperbolic_base_gen(multivariate_gen_hyperbolic_gen):
+    @abstractmethod
+    def _init_lamb(self, params: tuple):
+        pass
 
     def _check_params(self, params: tuple, **kwargs) -> None:
         # adjusting params to fit multivariate generalized hyperbolic params
-        if len(params) == 5:
+        num_params: int = len(params)
+        if num_params == 5:
             params = self._get_params(params, check_params=False)
-        elif len(params) != 6:
+        elif num_params != 6:
             raise ValueError("Incorrect number of params given by user")
-        self._num_params = 6
+        self._num_params = num_params
 
         # checking params
         super()._check_params(params)
@@ -27,7 +32,8 @@ class multivariate_marginal_hyperbolic_gen(multivariate_gen_hyperbolic_gen):
 
     def _get_params(self, params: Union[Params, tuple], **kwargs) -> tuple:
         params_tuple: tuple = PreFitContinuousMultivariate._get_params(self, params, **kwargs)
-        params_tuple = 1.0, *params_tuple
+        self._init_lamb(params_tuple)
+        params_tuple = self._lamb, *params_tuple
         return params_tuple[-6:]
 
     def _get_bounds(self, data: np.ndarray, as_tuple: bool = True, **kwargs) -> Union[dict, tuple]:
@@ -45,23 +51,23 @@ class multivariate_marginal_hyperbolic_gen(multivariate_gen_hyperbolic_gen):
         return self._get_params(adj_params, check_params=False)
 
     def _neg_q2(self, w_params: np.ndarray, etas: np.ndarray, deltas: np.ndarray, zetas: np.ndarray) -> float:
-        return super()._neg_q2((1.0, *w_params), etas, deltas, zetas)
+        return super()._neg_q2((self._lamb, *w_params), etas, deltas, zetas)
 
     def _q2_opt(self, bounds: tuple, etas: np.ndarray, deltas: np.ndarray, zetas: np.ndarray, q2_options: dict):
         q2_res = differential_evolution(self._neg_q2, bounds=bounds[:2], args=(etas, deltas, zetas), **q2_options)
         chi, psi = q2_res['x']
-        return {'x': np.array([1.0, chi, psi]), 'success': q2_res['success']}
+        return {'x': np.array([self._lamb, chi, psi]), 'success': q2_res['success']}
 
     def _gh_to_params(self, params: tuple) -> tuple:
         return params[1:]
 
     def _get_low_dim_theta0(self, data: np.ndarray, bounds: tuple) -> np.ndarray:
-        bounds = ((1.0, 1.0), *bounds)
+        bounds = ((self._lamb, self._lamb), *bounds)
         theta0: np.ndarray = super()._get_low_dim_theta0(data, bounds)
         return theta0[1:]
 
     def _low_dim_theta_to_params(self, theta: np.ndarray, S: np.ndarray, S_det: float) -> tuple:
-        theta = np.array([1.0, *theta], dtype=float)
+        theta = np.array([self._lamb, *theta], dtype=float)
         params: tuple = super()._low_dim_theta_to_params(theta=theta, S=S, S_det=S_det)
         return params[1:]
 
@@ -70,7 +76,25 @@ class multivariate_marginal_hyperbolic_gen(multivariate_gen_hyperbolic_gen):
         return {'chi': params[0], 'psi': params[1], 'loc': params[2], 'shape': params[3], 'gamma': params[4]}, params[2].size
 
 
+class multivariate_marginal_hyperbolic_gen(multivariate_hyperbolic_base_gen):
+    def _init_lamb(self, params: tuple):
+        self._lamb: float = 1.0
+
+
+class multivariate_hyperbolic_gen(multivariate_hyperbolic_base_gen):
+    def _init_lamb(self, params: tuple):
+        d: int = params[-1].shape[0]
+        self._lamb: float = 0.5 * (d + 1)
+
+
+class multivariate_nig_gen(multivariate_hyperbolic_base_gen):
+    def _init_lamb(self, params: tuple):
+        self._lamb: float = -0.5
+
+
 multivariate_marginal_hyperbolic: multivariate_marginal_hyperbolic_gen = multivariate_marginal_hyperbolic_gen(name='multivariate_marginal_hyperbolic', params_obj=MultivariateMarginalHyperbolicParams, num_params=5, max_num_variables=np.inf)
+multivariate_hyperbolic: multivariate_hyperbolic_gen = multivariate_hyperbolic_gen(name='multivariate_hyperbolic', params_obj=MultivariateHyperbolicParams, num_params=5, max_num_variables=np.inf)
+multivariate_nig: multivariate_nig_gen = multivariate_nig_gen(name='multivariate_nig', params_obj=MultivariateNIGParams, num_params=5, max_num_variables=np.inf)
 
 
 if __name__ == '__main__':
@@ -86,22 +110,26 @@ if __name__ == '__main__':
     my_psi = 4.5
     my_gamma = np.array([2.3, -4.3], dtype=float)
 
+    # dist = multivariate_marginal_hyperbolic
+    # dist = multivariate_hyperbolic
+    dist = multivariate_nig
+
     my_params = (my_chi, my_psi, my_loc, my_shape, my_gamma)
 
-    rvs = multivariate_marginal_hyperbolic.rvs(1000, my_params)
+    rvs = dist.rvs(1000, my_params)
     # print(rvs)
     # multivariate_marginal_hyperbolic.pdf_plot(params=my_params)
     # print(multivariate_marginal_hyperbolic.pdf(rvs, my_params))
 
-    my_marg_hyperbolic = multivariate_marginal_hyperbolic.fit(rvs, method='em', show_progress=True, min_retries=1, max_retries=1)
+    my_dist = dist.fit(rvs, show_progress=True, min_retries=1, max_retries=1, tol=0.1)
     # my_marg_hyperbolic = multivariate_marginal_hyperbolic.fit(rvs, method='low-dim mle', show_progress=True)
-    print('theoretical max: ', multivariate_marginal_hyperbolic.loglikelihood(rvs, my_params))
-    print(my_marg_hyperbolic.params.to_dict)
+    print('theoretical max: ', dist.loglikelihood(rvs, my_params))
+    print(my_dist.params.to_dict)
 
     import matplotlib.pyplot as plt
 
-    p1 = multivariate_marginal_hyperbolic.pdf(rvs, my_params)
-    p2 = my_marg_hyperbolic.pdf(rvs)
+    p1 = dist.pdf(rvs, my_params)
+    p2 = my_dist.pdf(rvs)
     fig = plt.figure()
     ax = fig.add_subplot(projection='3d')
     num = rvs.shape[0]
---
+++ sklarpy/multivariate/_distributions/_params/_hyperbolics.py
@@ -0,0 +1,29 @@
+from sklarpy.multivariate._distributions._params._generalized_hyperbolic import MultivariateGenHyperbolicParams
+
+
+__all__ = ['MultivariateMarginalHyperbolicParams', 'MultivariateHyperbolicParams', 'MultivariateNIGParams']
+
+
+class MultivariateMarginalHyperbolicParams(MultivariateGenHyperbolicParams):
+    @property
+    def lamb(self) -> float:
+        """The lambda parameter of the Multivariate Generalized Hyperbolic distribution
+        and the underlying GIG distribution for W."""
+        return 1.
+
+
+class MultivariateHyperbolicParams(MultivariateGenHyperbolicParams):
+    @property
+    def lamb(self) -> float:
+        """The lambda parameter of the Multivariate Generalized Hyperbolic distribution
+        and the underlying GIG distribution for W."""
+        d: int = self.loc.size
+        return 0.5 * (d + 1)
+
+
+class MultivariateNIGParams(MultivariateGenHyperbolicParams):
+    @property
+    def lamb(self) -> float:
+        """The lambda parameter of the Multivariate Generalized Hyperbolic distribution
+        and the underlying GIG distribution for W."""
+        return -0.5
