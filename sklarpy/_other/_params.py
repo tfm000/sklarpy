e9fdf884daa0d88ee2954414412f9a4b0ab0e353
tfm000 <tylerfmitchell1999@gmail.com>
2023-08-17 13:22:26 +0100

    allowing params to be indexable

--- sklarpy/_other/_params.py
+++ sklarpy/_other/_params.py
@@ -1,3 +1,5 @@
+from typing import Union
+
 from sklarpy._other._serialize import Savable
 
 __all__ = ['Params']
@@ -19,10 +21,12 @@ class Params(Savable):
     def __contains__(self, param_name: str) -> bool:
         return param_name in self.to_dict
 
-    def __getitem__(self, param_name: str):
-        if self.__contains__(param_name):
-            return self.to_dict[param_name]
-        raise KeyError(f"{param_name} not valid")
+    def __getitem__(self, index: Union[str, int]):
+        if self.__contains__(index):
+            return self.to_dict[index]
+        elif isinstance(index, int) and ((0 <= index < len(self)) or (-len(self) <= index < 0)):
+            return self.to_tuple[index]
+        raise KeyError(f"{index} not valid")
 
     def __str__(self):
         return f"{self.name.title()}Params"
