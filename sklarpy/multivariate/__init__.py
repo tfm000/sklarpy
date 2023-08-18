d4dc646da84ac2aa89990396e7770a29fde790d2
tfm000 <tylerfmitchell1999@gmail.com>
2023-08-18 21:34:03 +0100

    adding gen hyperbolic to imports

--- sklarpy/multivariate/__init__.py
+++ sklarpy/multivariate/__init__.py
@@ -1,3 +1,3 @@
-from sklarpy.multivariate.distributions import multivariate_gaussian_kde, multivariate_marginal_hyperbolic, multivariate_hyperbolic, \
+from sklarpy.multivariate.distributions import multivariate_gaussian_kde, multivariate_gen_hyperbolic, multivariate_marginal_hyperbolic, multivariate_hyperbolic, \
     multivariate_nig, multivariate_normal, multivariate_skewed_t, multivariate_student_t, multivariate_sym_gen_hyperbolic, \
     multivariate_sym_marginal_hyperbolic, multivariate_sym_hyperbolic, multivariate_sym_nig
--- sklarpy/multivariate/distributions.py
+++ sklarpy/multivariate/distributions.py
@@ -18,7 +18,7 @@ from sklarpy.multivariate._params._student_t import MultivariateStudentTParams
 from sklarpy.multivariate._params._symmetric_generalized_hyperbolic import MultivariateSymGenHyperbolicParams
 from sklarpy.multivariate._params._symmetric_hyperbolics import MultivariateSymMarginalHyperbolicParams, MultivariateSymHyperbolicParams, MultivariateSymNIGParams
 
-__all__ = ['multivariate_gaussian_kde', 'multivariate_marginal_hyperbolic', 'multivariate_hyperbolic', 'multivariate_nig',
+__all__ = ['multivariate_gaussian_kde', 'multivariate_gen_hyperbolic', 'multivariate_marginal_hyperbolic', 'multivariate_hyperbolic', 'multivariate_nig',
            'multivariate_normal', 'multivariate_skewed_t', 'multivariate_student_t', 'multivariate_sym_gen_hyperbolic',
            'multivariate_sym_marginal_hyperbolic', 'multivariate_sym_hyperbolic', 'multivariate_sym_nig']
 
