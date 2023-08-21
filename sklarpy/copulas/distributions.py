f086bf5ac8ae4647f5d488c88f5c5a8a819b1b7f
tfm000 <tylerfmitchell1999@gmail.com>
2023-08-21 14:40:45 +0100

    adding symmetric generalized hyperbolic copula

--- sklarpy/copulas/distributions.py
+++ sklarpy/copulas/distributions.py
@@ -3,8 +3,9 @@ from sklarpy.copulas._distributions._gaussian_kde import gaussian_kde_copula_gen
 from sklarpy.copulas._distributions._generalized_hyperbolic import gen_hyperbolic_copula_gen
 
 from sklarpy.copulas._distributions._student_t import student_t_copula_gen
+from sklarpy.copulas._distributions._symmetric_generalized_hyperbolic import sym_gen_hyperbolic_copula_gen
 
-from sklarpy.multivariate import multivariate_normal, multivariate_gaussian_kde, multivariate_gen_hyperbolic, multivariate_student_t
+from sklarpy.multivariate import multivariate_normal, multivariate_gaussian_kde, multivariate_gen_hyperbolic, multivariate_student_t, multivariate_sym_gen_hyperbolic
 
 __all__ = ['gaussian_copula', 'gaussian_kde_copula', 'student_t_copula']
 
@@ -21,3 +22,4 @@ gaussian_kde_copula: gaussian_kde_copula_gen = gaussian_kde_copula_gen(name="gau
 gen_hyperbolic_copula: gen_hyperbolic_copula_gen = gen_hyperbolic_copula_gen(name="gen_hyperbolic", mv_object=multivariate_gen_hyperbolic)
 
 student_t_copula: student_t_copula_gen = student_t_copula_gen(name="student_t", mv_object=multivariate_student_t)
+sym_gen_hyperbolic_copula: sym_gen_hyperbolic_copula_gen = sym_gen_hyperbolic_copula_gen(name="sym_gen_hyperbolic", mv_object=multivariate_sym_gen_hyperbolic)
