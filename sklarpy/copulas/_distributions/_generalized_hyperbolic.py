9e91e28415126fe804aad551c705e376c8622c2c
tfm000 <tylerfmitchell1999@gmail.com>
2023-08-21 14:10:20 +0100

    removing duplicate code

--- sklarpy/copulas/_distributions/_generalized_hyperbolic.py
+++ sklarpy/copulas/_distributions/_generalized_hyperbolic.py
@@ -1,5 +1,5 @@
 import numpy as np
-from typing import Union
+from typing import Union, Callable
 
 from sklarpy.copulas._prefit_dicts import PreFitCopula
 from sklarpy._other import Params
@@ -9,26 +9,21 @@ __all__ = ['gen_hyperbolic_copula_gen']
 
 
 class gen_hyperbolic_copula_gen(PreFitCopula):
-    def _g_to_u(self, g: np.ndarray, copula_params: Union[Params, tuple]) -> np.ndarray:
-        shape: tuple = g.shape
-        u: np.ndarray = np.full(shape, np.nan)
+
+    def __u_g_pdf(self, func: Callable, arr: np.ndarray, copula_params: Union[Params, tuple], **kwargs) -> np.ndarray:
+        shape: tuple = arr.shape
+        output: np.ndarray = np.full(shape, np.nan)
         for i in range(shape[1]):
             gh_params: tuple = copula_params[0], copula_params[1], copula_params[2], 0.0, 1.0, float(copula_params[-1][i])
-            u[:, i] = gh.cdf_approx(g[:, i], gh_params, num_points=10)
-        return u
+            output[:, i] = func(arr[:, i], gh_params, **kwargs)
+        return output
+
+    def _g_to_u(self, g: np.ndarray, copula_params: Union[Params, tuple]) -> np.ndarray:
+        return self.__u_g_pdf(func=gh.cdf_approx, arr=g, copula_params=copula_params, num_points=10)
 
     def _u_to_g(self, u: np.ndarray, copula_params: Union[Params, tuple]):
-        shape: tuple = u.shape
-        g: np.ndarray = np.full(shape, np.nan)
-        for i in range(shape[1]):
-            gh_params: tuple = copula_params[0], copula_params[1], copula_params[2], 0.0, 1.0, float(copula_params[-1][i])
-            g[:, i] = gh.ppf_approx(u[:, i], gh_params, num_points=10)
-        return g
+        return self.__u_g_pdf(func=gh.ppf_approx, arr=u, copula_params=copula_params, num_points=10)
 
     def _h_logpdf_sum(self, g: np.ndarray, copula_params: Union[Params, tuple]) -> np.ndarray:
-        shape: tuple = g.shape
-        pdf_vals: np.ndarray = np.full(shape, np.nan)
-        for i in range(shape[1]):
-            gh_params: tuple = copula_params[0], copula_params[1], copula_params[2], 0.0, 1.0, float(copula_params[-1][i])
-            pdf_vals[:, i] = gh.pdf(g[:, i], gh_params)
+        pdf_vals: np.ndarray = self.__u_g_pdf(func=gh.pdf, arr=g, copula_params=copula_params)
         return np.log(pdf_vals).sum(axis=1)
