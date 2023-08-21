502628d1fffdf8d32ea8239ad185f74760d96dfd
tfm000 <tylerfmitchell1999@gmail.com>
2023-08-21 19:34:16 +0100

    updating examples

--- examples/copula_examples/copula_example.py
+++ examples/copula_examples/copula_example.py
@@ -1,6 +1,10 @@
+# This file contains examples of how to use copula distributions in SklarPy.
+# Here we use the skewed-t copula, though all methods / attributes are generalized.
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
 
+# specifying the parameters of the multivariate hyperbolic distribution we are generating from
 my_loc = np.array([1, -3], dtype=float)
 my_shape = np.array([[1, 0.7], [0.7, 1]], dtype=float)
 my_chi = 1.7
@@ -8,20 +12,34 @@ my_psi = 4.5
 my_gamma = np.array([2.3, -4.3], dtype=float)
 my_params = (my_chi, my_psi, my_loc, my_shape, my_gamma)
 
+# generating multivariate hyperbolic random variables
 from sklarpy.multivariate import multivariate_hyperbolic
 
 num_generate: int = 1000
 rvs: np.ndarray = multivariate_hyperbolic.rvs(num_generate, my_params)
-rvs_df: pd.DataFrame = pd.DataFrame(rvs, columns=['Wife Age', 'Husband Age'], dtype=float)
-# multivariate_hyperbolic.pdf_plot(my_params)
-print(rvs_df)
+rvs_df: pd.DataFrame = pd.DataFrame(rvs, columns=['Process A', 'Process B'], dtype=float)
 
-from sklarpy.copulas import gaussian_copula as copula
-fitted_copula = copula.fit(data=rvs_df, univariate_fitter_options={'significant': False, 'distributions': ['student_t']})
-fitted_copula.pdf_plot()
-fitted_copula.copula_pdf_plot()
-# fitted_copula.pdf_plot(show=False)
-# multivariate_hyperbolic.pdf_plot(my_params, show=False)
-import matplotlib.pyplot as plt
+# fitting a skewed-t copula to our generated data
+from sklarpy.copulas import skewed_t_copula
+
+fitted_copula = skewed_t_copula.fit(data=rvs_df, method='low-dim mle', univariate_fitter_options={'pvalue': 0.0}, show_progress=True)
+print(fitted_copula.copula_params.to_dict)
+print(fitted_copula.mdists)
+
+# printing a summary of our fit
+print(fitted_copula.summary)
+
+# plotting our fit
+fitted_copula.copula_pdf_plot(show=False)
+fitted_copula.pdf_plot(show=False)
+fitted_copula.mc_cdf_plot(show=False)
 plt.show()
-# breakpoint()
+
+# we can also save our fitted copula, as well as its parameters
+fitted_copula.save()
+
+# we can then easily load this object
+from sklarpy import load
+
+loaded_copula = load('skewed_t_copula.pickle')
+print(loaded_copula.summary)
