e40b2cacd91fb0dfa2689e118fd486761e1543d2
tfm000 <tylerfmitchell1999@gmail.com>
2023-09-01 11:11:28 +0100

    making PEP8
    Took 1 minute

--- sklarpy/copulas/distributions.py
+++ sklarpy/copulas/distributions.py
@@ -45,38 +45,51 @@ gaussian_kde_copula: gaussian_kde_copula_gen = \
 ###############################################################################
 gaussian_copula: gaussian_copula_gen = \
     gaussian_copula_gen(name="gaussian", mv_object=multivariate_normal)
+
 gen_hyperbolic_copula: gen_hyperbolic_copula_gen = \
     gen_hyperbolic_copula_gen(
         name="gen_hyperbolic", mv_object=multivariate_gen_hyperbolic)
+
 marginal_hyperbolic_copula: marginal_hyperbolic_copula_gen = \
     marginal_hyperbolic_copula_gen(
         name="marginal_hyperbolic", mv_object=multivariate_marginal_hyperbolic)
+
 hyperbolic_copula: hyperbolic_copula_gen = \
     hyperbolic_copula_gen(
         name="hyperbolic", mv_object=multivariate_hyperbolic)
+
 nig_copula: nig_copula_gen = \
     nig_copula_gen(name="nig", mv_object=multivariate_nig)
+
 skewed_t_copula: skewed_t_copula_gen = \
     skewed_t_copula_gen(
         name="skewed_t", mv_object=multivariate_skewed_t)
+
 student_t_copula: student_t_copula_gen = \
     student_t_copula_gen(
         name="student_t", mv_object=multivariate_student_t)
+
 sym_gen_hyperbolic_copula: sym_gen_hyperbolic_copula_gen = \
     sym_gen_hyperbolic_copula_gen(
         name="sym_gen_hyperbolic", mv_object=multivariate_sym_gen_hyperbolic)
+
 sym_marginal_hyperbolic_copula: sym_marginal_hyperbolic_copula_gen = \
     sym_marginal_hyperbolic_copula_gen(
         name="sym_marginal_hyperbolic",
         mv_object=multivariate_sym_marginal_hyperbolic)
+
 sym_hyperbolic_copula: sym_hyperbolic_copula_gen = \
     sym_hyperbolic_copula_gen(name="sym_hyperbolic",
                               mv_object=multivariate_sym_hyperbolic)
+
 sym_nig_copula: sym_nig_copula_gen = \
     sym_nig_copula_gen(name="sym_nig", mv_object=multivariate_sym_nig)
+
 clayton_copula: clayton_copula_gen = \
     clayton_copula_gen(name="clayton", mv_object=multivariate_clayton)
+
 gumbel_copula: gumbel_copula_gen = \
     gumbel_copula_gen(name="gumbel", mv_object=multivariate_gumbel)
+
 frank_copula: frank_copula_gen = \
     frank_copula_gen(name="frank", mv_object=bivariate_frank)
