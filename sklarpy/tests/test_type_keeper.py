3b6260805bc047bc238221a08edf526d08d3bc95
Tyler <tylerfmitchell@gmail.com>
2023-11-03 22:19:30 +0000

    removing old test files

--- sklarpy/tests/test_type_keeper.py
+++
@@ -1,8 +0,0 @@
-# import pandas as pd
-# import numpy as np
-#
-# from sklarpy._utils._type_keeper_v2 import TypeKeeperV2 as TyperKeeper
-#
-#
-# def type_keep_from_1d_array(arr_1d: np.ndarray, df_1d: pd.DataFrame):
-#     pass
