675e68cfde23756858f1cd9f20f9001dd7ba0850
Tyler <tylerfmitchell@gmail.com>
2023-12-04 15:07:38 +0000

    adding sphinx auto doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,7 +2,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+# sys.path.insert(0, os.path.abspath(os.path.join('..', '..')))
+sys.path.insert(0, os.path.abspath('../../sklarpy') )
+
 
 # -- Project information
 
